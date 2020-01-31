dcde7bec8f08bea10db7bffbb7186b23e044d6ea
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-01-31 22:07:52 +0000

    SAM Analytical Panel to BHoM added (with connectedSpaces parameter)

---
+++ Grasshopper/SAM.Analytical.Grasshopper.BHoM/Component/SAMAnalyticalPanelBHoM.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+
+using SAM.Core;
+using SAM.Analytical.Grasshopper.BHoM.Properties;
+using System.Collections.Generic;
+
+namespace SAM.Analytical.Grasshopper.BHoM
+{
+    public class SAMAnalyticalPanelBHoM : GH_Component
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("f3018f46-4706-4ea0-beb3-1fbca8b6ed47");
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMAnalyticalPanelBHoM()
+          : base("SAMAnalyticalPanel.BHoM", "SAMAnalyticalPanel.BHoM",
+              "Convert SAM Analytical Panel, Space to BHoM",
+              "SAM", "BHoM")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddParameter(new GooPanelParam(), "_panel", "_panel", "SAM Analytical Panel", GH_ParamAccess.item);
+            inputParamManager.AddTextParameter("_connectedSpaces", "_connectedSpaces", "Connected Spaces Names", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddGenericParameter("BHoMPanel", "BHoMPanel", "BHoM Panel", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            Panel panel = null;
+            if (!dataAccess.GetData(0, ref panel) || panel == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<string> connectedSpaces = new List<string>();
+            if (!dataAccess.GetData(1, ref connectedSpaces) || connectedSpaces == null)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            dataAccess.SetData(0, Analytical.BHoM.Convert.ToBHoM(panel, connectedSpaces));
+            return;
+        }
+    }
+}
--- SAM_BHoM/SAM.Analytical.BHoM/ToBHoM/Panel.cs
+++ SAM_BHoM/SAM.Analytical.BHoM/ToBHoM/Panel.cs
@@ -1,6 +1,7 @@
 ﻿using BH.Engine.Environment;
 
 using SAM.Geometry.BHoM;
+using System.Collections.Generic;
 
 namespace SAM.Analytical.BHoM
 {
@@ -16,6 +17,20 @@ namespace SAM.Analytical.BHoM
             {
                 ExternalEdges = polygon3D.ToBHoM().ToEdges(),
                 Type = panel.PanelType.ToBHoM(),
+            };
+        }
+
+        public static BH.oM.Environment.Elements.Panel ToBHoM(this Panel panel, List<string> connectedSpaces)
+        {
+            PlanarBoundary3D planarBoundary3D = panel.PlanarBoundary3D;
+            Geometry.Spatial.Face face = planarBoundary3D.GetFace();
+            Geometry.Spatial.Polygon3D polygon3D = face.ToClosedPlanar3D() as Geometry.Spatial.Polygon3D;
+
+            return new BH.oM.Environment.Elements.Panel
+            {
+                ExternalEdges = polygon3D.ToBHoM().ToEdges(),
+                Type = panel.PanelType.ToBHoM(),
+                ConnectedSpaces = connectedSpaces,
             };
         }
     }

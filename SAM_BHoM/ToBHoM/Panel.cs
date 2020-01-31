978cdfb01946966dcd1f7101553786c33afd33f1
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2020-01-31 12:19:00 +0000

    Minor updates with new structure

--- SAM_BHoM/ToBHoM/Panel.cs
+++ SAM_BHoM/ToBHoM/Panel.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using BHE = BH.oM.Environment.Elements;
+﻿using BHE = BH.oM.Environment.Elements;
 using SAMA = SAM.Analytical;
-using BHG = BH.oM.Geometry;
 using SAMG = SAM.Geometry;
 
 using BH.Engine.Environment;
-using BH.Engine.Geometry;
 
 namespace BH.Engine.SAM
 {
@@ -18,7 +10,7 @@ namespace BH.Engine.SAM
     {
         public static BHE.Panel ToBHoM(this SAMA.Panel panel)
         {
-            SAMA.Boundary3D bounds = panel.Boundary3D;
+            SAMA.PlanarBoundary3D bounds = panel.PlanarBoundary3D;
             SAMG.Spatial.Face face = bounds.GetFace();
             SAMG.Spatial.Polygon3D pGon = face.ToClosedPlanar3D() as SAMG.Spatial.Polygon3D;
 
@@ -33,7 +25,7 @@ namespace BH.Engine.SAM
         {
             SAMG.Spatial.Polygon3D pGon = panel.Polyline().ToSAM();
             SAMG.Spatial.Face face = new SAMG.Spatial.Face(pGon);
-            SAMA.Boundary3D bounds = new SAMA.Boundary3D(face);
+            SAMA.PlanarBoundary3D bounds = new SAMA.PlanarBoundary3D(face);
 
             return new SAMA.Panel(null, panel.Type.ToSAM(), bounds);
         }

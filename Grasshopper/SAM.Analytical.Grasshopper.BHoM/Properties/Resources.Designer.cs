ce669f6cc696a3683523368121bcbb9b7dd09c41
Michal Dengusiak <tassimulation@gmail.com>
2020-01-31 17:19:10 +0000

    fix icon

--- Grasshopper/SAM.Analytical.Grasshopper.BHoM/Properties/Resources.Designer.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.BHoM/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace SAM.Analytical.Grasshopper.BHoM.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap SAM_BHoM {
+            get {
+                object obj = ResourceManager.GetObject("SAM_BHoM", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap SAM_Small {
             get {
                 object obj = ResourceManager.GetObject("SAM_Small", resourceCulture);

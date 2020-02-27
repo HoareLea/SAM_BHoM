058d148b25fe664a1eca3c7ca6bbf0c9d09249cb
Michal Dengusiak <tassimulation@gmail.com>
2020-02-27 17:29:34 +0000

    Update Opening.cs

--- SAM_BHoM/SAM.Analytical.BHoM/ToBHoM/Opening.cs
+++ SAM_BHoM/SAM.Analytical.BHoM/ToBHoM/Opening.cs
@@ -8,7 +8,7 @@ namespace SAM.Analytical.BHoM
     {
         public static BH.oM.Environment.Elements.Opening ToBHoM(this Aperture aperture)
         {
-            PlanarBoundary3D planarBoundary3D = aperture.GetPlanarBoundary3D();
+            PlanarBoundary3D planarBoundary3D = aperture.PlanarBoundary3D;
             Geometry.Spatial.Face3D face3D = planarBoundary3D.GetFace3D();
             Geometry.Spatial.Polygon3D polygon3D = face3D.GetExternalEdge() as Geometry.Spatial.Polygon3D;
 

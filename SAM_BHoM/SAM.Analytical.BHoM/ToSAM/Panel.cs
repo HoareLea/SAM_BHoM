2c4393aee92acc3acdeaf4ecb80bd429fc5cbe8a
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-05-05 09:01:12 +0100

    Panel Constructors Moved to Create Namespace

--- SAM_BHoM/SAM.Analytical.BHoM/ToSAM/Panel.cs
+++ SAM_BHoM/SAM.Analytical.BHoM/ToSAM/Panel.cs
@@ -8,9 +8,9 @@ namespace SAM.Analytical.BHoM
         {
             Geometry.Spatial.Polygon3D polygon3D = Geometry.BHoM.Convert.ToSAM(panel.Polyline());
             Geometry.Spatial.Face3D face3D = new Geometry.Spatial.Face3D(polygon3D);
-            PlanarBoundary3D planarBoundary3D = new PlanarBoundary3D(face3D);
+            //PlanarBoundary3D planarBoundary3D = new PlanarBoundary3D(face3D);
 
-            return new Panel(null, panel.Type.ToSAM(), planarBoundary3D);
+            return Analytical.Create.Panel(null, panel.Type.ToSAM(), face3D);
         }
     }
 }
